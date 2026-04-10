02f2819c06e49cd5418f3cfb2041448812fe1720
Nicolas Djeghri <nicolas.djeghri@gmail.com>
2026-04-10 18:37:16 +0200

    "Full" documentation?
    Present function have a first full version of a documentation.
    Some changes to keep tidying and figuring out all the things around the package formatting.

--- R/CycloClust-package.R
+++ R/CycloClust-package.R
@@ -1,6 +1,7 @@
 #' CycloClust: Cyclical-time constrained clustering
 #'
-#' Constrained clustering of time series with cyclical dynamics (seasonality, day-night cycles etc.).
+#' Constrained clustering of time series with cyclical dynamics
+#' (seasonality, day-night cycles etc.).
 #'
 #' @name CycloClust-package
 #' @aliases CycloClust CycloClust-package
@@ -9,13 +10,22 @@
 #' \email{nicolas.djeghri@@gmail.com}
 #' [ORCID](https://orcid.org/0000-0001-5740-3386)
 #'
+#' @details
+#' The main function of the package is \code{\link{cyclo.hclust}}, derived from
+#' \code{\link{constr.hclust}}, and allowing to perform cyclical-time
+#' constrained hierarchical clustering. Once a clustering is obtained,
+#' \code{CycloClust} provides functions to follow changes in cluster position
+#' within cycles (see \code{\link{cluster.cyclo.pos}}, \code{\link{lin.cyclo.ts}})
+#' and to visualize clusters (A RAJOUTER!!!).
 #'
-#' @seealso \link{\code{adespatial}}
+#'
+#' @seealso \code{\link{adespatial}}
 #'
 #' @keywords internal
 "_PACKAGE"
 
 ## usethis namespace: start
 #' @importFrom stats dist as.dist cutree
+#' @importFrom adespatial constr.hclust
 ## usethis namespace: end
 NULL
--- R/cluster.cyclo.pos.R
+++ R/cluster.cyclo.pos.R
@@ -1,14 +1,27 @@
 #' Clusters' cyclical start, end, mean and range
 #'
-#' Function \code{cluster.cyclo.pos} computes the start, end, mean and range of cyclical-time-constrained clusters along the time series cycles.
+#' Function \code{cluster.cyclo.pos} computes indices of cluster position
+#' (start, end, mean and range) of cyclical-time-constrained clusters along
+#' in different cycles.
 #'
 #' @details
+#' A valuable aspect of a cyclical-time constrained clustering is the ability to
+#' compare clusters' position in several cycles. Displacement of clusters can be
+#' interpreted as advancing or delaying of a particular state along the cycles.
+#' The function \code{cluster.cyclo.pos} compute four indices allowing related to
+#' cluster position within cycles (see Value).
 #'
 #' @return
+#' An object of class \code{\link{list}}, containing:
+#' \itemize{
+#'  \item{\code{means}: A table containing the mean position of each cluster in each cycle.}
+#'  \item{\code{starts}: A table containing the starting position of each cluster along each cycle.}
+#'  \item{\code{ends}: A table containing the ending position of each cluster along each cycle.}
+#'  \item{\code{ranges}: A table containing the ranges covered by each cluster within each cycle.}
+#' }
 #'
 #' @author Nicolas Djeghri, UBO
 #'
-#' @references
 #'
 #' @encoding UTF-8
 #' @name cluster.cyclo.pos
@@ -16,6 +29,7 @@
 #' @seealso \code{\link{cyclo.hclust}}
 #'
 #' @examples
+#' #WRITE SOME R CODE IN THERE!!!
 #'
 #' @param x A cyclical-time-constrained clustering object, as provided by function \code{\link{cyclo.hclust}}.
 #' @param k an integer scalar with the desired number of clusters (as in \code{\link{cutree}}).
@@ -44,30 +58,30 @@ cluster.cyclo.pos <- function(x,
     anglesi <- angles.cyclic.time[index]
 
     if (length(unique(groupsi))>1){
-      means <- ((tapply(anglesi,groupsi,circ.mean)%%(2*pi))/(2*pi))*x$cycle.duration
+      means <- ((tapply(anglesi,groupsi,cyclo.mean)%%(2*pi))/(2*pi))*x$cycle.duration
       cycleMeans[as.character(i),] <- means[as.character(1:length(unique(groups)))]
 
       for (j in 1:length(groupsi)){
         if (j == 1){
           if (groupsi[j]!=groupsi[length(groupsi)]){
-            cycleStarts[as.character(i),as.character(groupsi[j])] <- circ.mean(anglesi[c(1,length(groupsi))])
+            cycleStarts[as.character(i),as.character(groupsi[j])] <- cyclo.mean(anglesi[c(1,length(groupsi))])
           }
           if (groupsi[j]!=groupsi[j+1]){
-            cycleEnds[as.character(i),as.character(groupsi[j])] <- circ.mean(anglesi[1:2])
+            cycleEnds[as.character(i),as.character(groupsi[j])] <- cyclo.mean(anglesi[1:2])
           }
         }else if (j == length(groupsi)){
           if (groupsi[j]!=groupsi[j-1]){
-            cycleStarts[as.character(i),as.character(groupsi[j])] <- circ.mean(anglesi[c(j,j-1)])
+            cycleStarts[as.character(i),as.character(groupsi[j])] <- cyclo.mean(anglesi[c(j,j-1)])
           }
           if (groupsi[j]!=groupsi[1]){
-            cycleEnds[as.character(i),as.character(groupsi[j])] <- circ.mean(anglesi[c(j,1)])
+            cycleEnds[as.character(i),as.character(groupsi[j])] <- cyclo.mean(anglesi[c(j,1)])
           }
         }else{
           if (groupsi[j]!=groupsi[j-1]){
-            cycleStarts[as.character(i),as.character(groupsi[j])] <- circ.mean(anglesi[c(j,j-1)])
+            cycleStarts[as.character(i),as.character(groupsi[j])] <- cyclo.mean(anglesi[c(j,j-1)])
           }
           if (groupsi[j]!=groupsi[j+1]){
-            cycleEnds[as.character(i),as.character(groupsi[j])] <- circ.mean(anglesi[c(j,j+1)])
+            cycleEnds[as.character(i),as.character(groupsi[j])] <- cyclo.mean(anglesi[c(j,j+1)])
           }
         }
       }
@@ -88,6 +102,7 @@ cluster.cyclo.pos <- function(x,
   output$ends <- cycleEnds
   output$ranges <- cycleRanges
 
+  class(output) <- c("list")
   return(output)
 }
 
